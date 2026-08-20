9fe1b2b1dbcc96b8a23a4dba8ba88ab2d2e916ae
pedropersici <pedropersici@gmail.com>
2026-08-20 17:25:19 -0300

    Add password confirmation feature in free4.py

---
+++ free4.py
@@ -0,0 +1,6 @@
+n1 = str(input('Enter the password: '))
+n2 = str(input('Enter the password again: '))
+if n1 == n2:
+    print('The password is correct.')
+else:
+    print('The password is incorrect.')
